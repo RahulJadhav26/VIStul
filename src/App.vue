44412343169e48d6948683ed440d93938ae49839
RahulJadhav2611 <rrahuljadhav.rj@gmail.com>
2020-08-17 01:35:46 +0530

    Collection

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@ export default {
 </script>
 
 <style>
+
 @import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap');
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
@@ -21,6 +22,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+
 }
 .slide-enter-active{
 animation: slide-in 200ms ease-out forwards;
